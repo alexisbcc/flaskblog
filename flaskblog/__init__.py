55cc9395944b8cc7fee662db363e67c90a69acb1
Alexis Crespo <alexis-2cab@hotmail.com>
2020-04-12 15:56:11 -0500

    custom error handlers

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -6,7 +6,6 @@ from flask_mail import Mail
 from flaskblog.config import Config
 
 
-
 db = SQLAlchemy()
 mail = Mail()
 bcrypt = Bcrypt()
@@ -27,9 +26,11 @@ def create_app(config_class=Config):
     from flaskblog.users.routes import users
     from flaskblog.posts.routes import posts
     from flaskblog.main.routes import main
+    from flaskblog.errors.handlers import errors
 
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
